ca4451f45a29bf112d14159066e1be3d8bb68e15
Zears14 <79321017+Zears14@users.noreply.github.com>
2025-05-29 16:49:57 +0700

    Removes unused trace events and fixes block validity checks
    This commit removes unused trace events related to hps_update and perf_index_l from mtk_events.h, reducing unnecessary overhead.
    
    It also refactors the ext4 filesystem code to use `ext4_inode_block_valid` for checking block validity, ensuring inode-specific checks are performed. This addresses potential issues with generic data block validity checks.
    The trace event code was likely commented out to disable debug traces.
    Additionally, the root inode link count check has been removed from `__ext4_iget`.
    
    Signed-off-by: Zears14 <79321017+Zears14@users.noreply.github.com>

--- drivers/misc/mediatek/performance/resym/rs_index.c
+++ drivers/misc/mediatek/performance/resym/rs_index.c
@@ -290,7 +290,7 @@ static void rs_start_collect(void)
 
 	spin_unlock_irqrestore(&rs_loading_slock, flags);
 
-	register_trace_perf_index_l(rs_update_io_stat, NULL);
+	//register_trace_perf_index_l(rs_update_io_stat, NULL);
 
 	rsi_get_data(&sysdata);
 }
@@ -312,7 +312,7 @@ static void rs_stop_collect(void)
 	rs_reset_io_list_locked();
 	spin_unlock_irqrestore(&rs_loading_slock, flags);
 
-	unregister_trace_perf_index_l(rs_update_io_stat, NULL);
+	//unregister_trace_perf_index_l(rs_update_io_stat, NULL);
 
 	prev_ts = 0;
 }
